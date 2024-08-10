918e49f888baac0c8cd2ebe6bf497f11d77a7479
Ryan Bynoe <73267001+ryanbynoe@users.noreply.github.com>
2024-08-10 08:51:43 -0400

    Added terraform files

---
+++ main.tf
@@ -0,0 +1,39 @@
+# main.tf
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = var.vm_size  # Uses the VM size variable
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "user_pool" {
+  name                  = "userpool"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
+  vm_size               = var.vm_size  # Uses the same VM size variable
+  node_count            = 1
+  mode                  = "User"
+
+  node_labels = {
+    "nodepool-type" = "user-nodes"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+# variables.tf
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group"
+  type        = string
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "DNS prefix for the AKS cluster"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the VM instances in the AKS cluster"
+  type        = string
+}
